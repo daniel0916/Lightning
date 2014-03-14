83c743b63d8452b5be6ed3bc8fd194e807ae7713
daniel0916 <theschokolps@gmail.com>
2014-03-14 19:29:41 +0100

    Added BlazeRod RightClicking

---
+++ hook.lua
@@ -0,0 +1,7 @@
+function OnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ)
+	if Player:GetEquippedItem().m_ItemType == E_ITEM_BLAZE_ROD then
+		if Player:HasPermission("lightning.use") then
+			Player:GetWorld():CastThunderbolt(GetBlockXYZFromTrace(Player))
+		end
+	end
+end
--- main.lua
+++ main.lua
@@ -5,6 +5,18 @@ function Initialize(Plugin)
 	local PluginManager = cRoot:Get():GetPluginManager();
     PluginManager:BindCommand("/lightning", 	"lightning.use", 	HandleLightningCommand, 	 " - Create Lightnings");
 	
+	cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_RIGHT_CLICK, OnPlayerRightClick)
+	
     LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion());
     return true;
+end
+
+function GetBlockXYZFromTrace(Player)
+	local World = Player:GetWorld()
+	local Tracer = cTracer(World)
+
+	local EyePos = Vector3f(Player:GetEyePosition().x, Player:GetEyePosition().y, Player:GetEyePosition().z)
+	local EyeVector = Vector3f(Player:GetLookVector().x, Player:GetLookVector().y, Player:GetLookVector().z)
+	Tracer:Trace(EyePos , EyeVector, 50)
+	return Tracer.BlockHitPosition.x, Tracer.BlockHitPosition.y, Tracer.BlockHitPosition.z
 end
