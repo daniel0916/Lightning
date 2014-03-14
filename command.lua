e9a07ecc49859c777118b6afecff7e9d6a9a2147
daniel0916 <theschokolps@gmail.com>
2014-03-14 17:48:39 +0100

    Added Lightning to Players

--- command.lua
+++ command.lua
@@ -1,5 +1,22 @@
 function HandleLightningCommand(Split, Player)
-	Player:GetWorld():CastThunderbolt(GetBlockXYZFromTrace(Player))
+	if #Split > 2 then
+		Player:SendMessage(cChatColor.Rose .. "Too many arguments.")
+		Player:SendMessage(cChatColor.Rose .. "Use /lightning or /lightning <PlayerName>")
+		return true
+	end
+	
+	if #Split == 1 then
+		Player:GetWorld():CastThunderbolt(GetBlockXYZFromTrace(Player))
+	elseif #Split == 2 then
+		if Player:GetWorld():DoWithPlayer(Split[2],
+			function(a_Player)
+				a_Player:GetWorld():CastThunderbolt(a_Player:GetPosX(), a_Player:GetPosY(), a_Player:GetPosZ())
+			end
+		) == false then
+			Player:SendMessage(cChatColor.Rose .. "Player couldn't be found.")
+			return true
+		end
+	end
 	
 	Player:SendMessage(cChatColor.Blue .. "Lightning created!")
 	return true
